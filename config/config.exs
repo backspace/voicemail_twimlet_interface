29ea7ed0eb659215947b574856b2a684859fbd2b
Buck Doyle <b@chromatin.ca>
2016-10-18 00:21:47 +0200

    Add Twilio configuration via environment variables

--- config/config.exs
+++ config/config.exs
@@ -18,6 +18,9 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :ex_twilio, account_sid: System.get_env("TWILIO_ACCOUNT_SID"),
+                   auth_token: System.get_env("TWILIO_AUTH_TOKEN")
+
 config :voicemail_twimlet_interface,
   number_sid: System.get_env("TWILIO_NUMBER_SID")
 
